6ca04ab96adf3ba1d9661f8aae1a22c06c68cf2d
Abderrahmane Fouzi ABOUZAID <Abderrahmane.ABOUZAID@um6p.ma>
2026-03-19 22:13:07 +0000

    removing useless files

--- backend/src/middlewares/notFound.middleware.js
+++
@@ -1,8 +0,0 @@
-function notFoundMiddleware(req, res, next) {
-  res.status(404).json({
-    success: false,
-    message: `Route not found: ${req.originalUrl}`,
-  });
-}
-
-module.exports = notFoundMiddleware;
